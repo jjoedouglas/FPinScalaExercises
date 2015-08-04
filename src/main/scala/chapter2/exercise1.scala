b7e10bcf8c5104e2b6e2e1813049a2d61f0bc26c
Joe <j.joe.douglas@gmail.com>
2015-08-03 21:24:01 -0400

    Chapter 2 exercise 1

---
+++ src/main/scala/chapter2/exercise1.scala
@@ -0,0 +1,15 @@
+package com.jjoedouglas.FPinScalaExercises.chapter2
+
+import scala.annotation.tailrec
+
+object Exercise1 {
+  def fib(n: Int): Int = {
+    @tailrec def fibHelper(n: Int, a: Int, b: Int): Int =
+      if (n == 1 || n == 2) return b
+      else fibHelper(n - 1, b, a+b)
+
+
+    if(n < 0) 0
+    else fibHelper(n, 1, 1)
+  }
+}
---
+++ src/test/scala/chapter2/exercise1.scala
@@ -0,0 +1,21 @@
+package com.jjoedouglas.FPinScalaExercises.chapter2
+
+import org.scalatest._
+
+class Exercise1Spec extends FlatSpec {
+  "fib(n)" must "equal 0 for negative values of n" in {
+    assert(Exercise1.fib(-1) == 0)
+  }
+
+  "fib(n)" must "equal 1 when n = 1" in {
+    assert(Exercise1.fib(1) == 1)
+  }
+
+  "fib(n)" must "equal 1 when n = 2" in {
+    assert(Exercise1.fib(2) == 1)
+  }
+
+  "fib(n)" should "equal 55 when n = 10" in {
+    assert(Exercise1.fib(10) == 55)
+  }
+}
