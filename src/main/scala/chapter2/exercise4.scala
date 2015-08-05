90b65ffa9bff96d997c4a6087e324202573ed25e
Joe <j.joe.douglas@gmail.com>
2015-08-05 19:25:57 -0400

    Exercises 3,4,5

---
+++ src/main/scala/chapter2/exercise4.scala
@@ -0,0 +1,6 @@
+package com.jjoedouglas.FPinScalaExercises.chapter2
+
+object Exercise4 {
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+}
---
+++ src/main/scala/chapter2/exercise5.scala
@@ -0,0 +1,5 @@
+package com.jjoedouglas.FPinScalaExercises.chapter2
+
+object Exercise5 {
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+}
